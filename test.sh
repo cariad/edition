da895cf497a5f47847f5291d5c6d93c484e3d0cc
Cariad Eccleston <cariad@cariad.earth>
2021-11-11 08:46:59 +0000

    Press README.md

--- test.sh
+++ test.sh
@@ -2,3 +2,9 @@
 set -euo pipefail
 
 pytest -vv
+
+if [ "${CI:=}" == "true" ]; then
+  echo "Skipping documenation press in CI."
+else
+  python -m edition docs/src.md --press markdown > README.md
+fi
