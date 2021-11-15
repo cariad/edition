e42abf4fb11c8e5316e5689f1ac41cac03c4837e
Cariad Eccleston <cariad@cariad.earth>
2021-11-15 16:47:52 +0000

    Fix test

--- scripts/test-cli.sh
+++ scripts/test-cli.sh
@@ -16,5 +16,5 @@ assert "$(edition --version || true)" "${CIRCLE_TAG:-"-1.-1.-1"}"
 edition docs/source.md docs/test.html --press html
 cmp --silent docs/index.html docs/test.html
 
-edition docs/source.md test.md --press html
+edition docs/source.md test.md --press markdown
 cmp --silent README.md test.md
