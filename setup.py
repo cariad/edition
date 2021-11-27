e8c983f4c0184dc078864a5a5af440c25202d992
Cariad Eccleston <cariad@cariad.earth>
2021-11-27 12:06:10 +0000

    Upgrade dinject

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     install_requires=[
         "cline~=1.0",
         "comprehemd==1.0.0a2",
-        "dinject~=1.2",
+        "dinject==1.3.0a1",
         "markdown~=3.3.4",
         "pygments~=2.10",
         "python-frontmatter~=1.0",
